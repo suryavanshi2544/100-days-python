ab6bbe56884310ef7da36d2fe841ff5ebb1b3c2d
vipin suryavanshi <vipinsuryavanshi@vipins-MacBook-Air.local>
2023-08-15 19:04:24 +0530

    add

--- 1-days.py
+++ 1-days.py
@@ -1,1 +1,8 @@
-print("hello")
+#print commamd
+print ("Hello, World!")
+
+#and save file with .py extension
+
+#check more basic syntax 
+
+# https://www.tutorialspoint.com/python/python_basic_syntax.htm
